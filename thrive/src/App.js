48c1f66e65ffe677b881823d324b347d6b12fd27
Renee Ibeji <reneeibeji@gmail.com>
2024-07-06 16:01:29 +0100

    Started creating a login page

--- thrive/src/App.js
+++ thrive/src/App.js
@@ -1,8 +1,10 @@
 import './App.css';
+import Login from './Login/Login.js';
 
 function App() {
   return (
     <div className="App">
+      <Login></Login>
     </div>
   );
 }
---
+++ thrive/src/Login/Login.js
@@ -0,0 +1,16 @@
+import React from "react";
+import Apptitle from "../Components/Apptitle";
+import Button from "../Components/button";
+
+export default class homepage extends React.Component{
+    render() {
+        return(
+            <html>
+                <Apptitle></Apptitle>
+                <Button name="Login"></Button>
+                <Button name="Register"></Button>
+                
+            </html>
+        );
+    }
+}
