f9607487b58535d461e4823e24378ce5258d6913
Renee Ibeji <reneeibeji@gmail.com>
2024-07-07 11:58:36 +0100

    lol

--- thrive/src/Components/Navbar.jsx
+++ thrive/src/Components/Navbar.jsx
@@ -1,14 +1,17 @@
 import { Link } from "react-router-dom"
+import "../CSS/Navbar.css"
 
 function Navbar() {
 
 return(
-  <div className="navbar">
-    <p>
+  <div>
+    <p>                           
       <img src="./Assets/Logo.png"></img>
     </p>
+  <div className="navbar" id="navbar">
     <button><Link to="/home">Home</Link></button>
     <button ><Link to="/community">Community</Link></button>
+  </div>
   </div>
 )
 }
